8079a2bd420b0bea19258db2a5188cb8658348d6
Dan Moffat <danmoffat@gmail.com>
2025-08-07 14:10:06 +0100

    A little styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { FiFacebook } from 'react-icons/fi';
-import { FiInstagram } from 'react-icons/fi';
+import { SiFacebook } from 'react-icons/si';
+import { SiInstagram } from 'react-icons/si';
 import logoSVG from '@/../public/logo.svg';
 import HomeSlider from '@/components/home-slider';
 
@@ -9,31 +9,40 @@ export default function Home() {
   return (
     <div className='absolute flex h-screen w-screen items-center justify-center'>
       <HomeSlider />
-      <div className='absolute inset-0 z-10 flex flex-col justify-between bg-linear-to-r from-black/50 to-transparent p-20'>
-        <Image src={logoSVG} alt='Harrison Jack Photography Logo' className='w-40 invert' />
-        <div>
-          <div className='mb-8 text-8xl font-light text-white'>
-            SOME
-            <br />
-            MARKETING
-            <br />
-            BOLLOCKS
+      <div className='absolute inset-0 z-10 bg-linear-to-r from-black/50 to-transparent p-20'>
+        <div className='flex h-full flex-col justify-between'>
+          <Image src={logoSVG} alt='Harrison Jack Photography Logo' className='w-40 invert' />
+          <div className='max-w-fit'>
+            <div className='mb-8 text-7xl leading-20 font-semibold text-white'>
+              How We
+              <br />
+              See Things
+            </div>
+            <div className='flex gap-4'>
+              <Link
+                href=''
+                className='grow bg-white py-1 text-center tracking-wider text-black uppercase opacity-90 hover:opacity-100'
+              >
+                About
+              </Link>
+              <Link
+                href=''
+                className='grow bg-white py-1 text-center tracking-wider text-black uppercase opacity-90 hover:opacity-100'
+              >
+                Folios
+              </Link>
+              <Link
+                href=''
+                className='grow bg-white py-1 text-center tracking-wider text-black uppercase opacity-90 hover:opacity-100'
+              >
+                Contact
+              </Link>
+            </div>
           </div>
           <div className='flex gap-4'>
-            <Link href='' className='bg-white px-6 py-1 text-black uppercase'>
-              About
-            </Link>
-            <Link href='' className='bg-white px-6 py-1 text-black uppercase'>
-              Folios
-            </Link>
-            <Link href='' className='bg-white px-6 py-1 text-black uppercase'>
-              Contact
-            </Link>
+            <SiFacebook className='size-7 opacity-90 hover:opacity-100' />
+            <SiInstagram className='size-7 opacity-90 hover:opacity-100' />
           </div>
-        </div>
-        <div className='flex gap-4'>
-          <FiFacebook className='size-8' />
-          <FiInstagram className='size-8' />
         </div>
       </div>
     </div>
