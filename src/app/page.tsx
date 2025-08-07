561722cef187febec1d90be7f1e094835d328a6a
Dan Moffat <danmoffat@gmail.com>
2025-08-07 12:13:51 +0100

    Added a home-slider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import HomeSlider from '@/components/home-slider';
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <HomeSlider />
+    </div>
+  );
 }
---
+++ src/components/home-slider.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function HomeSlider() {
+  return <div>Home Slider</div>;
+}
