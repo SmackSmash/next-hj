e86c8147eb958066b2c7f80e9218efb8be66563c
Dan Moffat <danmoffat@gmail.com>
2025-08-07 12:42:32 +0100

    Added fullscreen slider

--- src/components/home-slider.tsx
+++ src/components/home-slider.tsx
@@ -1,5 +1,52 @@
 'use client';
 
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectFade, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+
+import camelsImage from '@/../public/home/camels.jpg';
+import balconyImage from '@/../public/home/balcony.jpg';
+import chaletImage from '@/../public/home/chalet.jpg';
+import garlicImage from '@/../public/home/garlic.jpg';
+import lampshadesImage from '@/../public/home/lampshades.jpg';
+import skyImage from '@/../public/home/sky.jpg';
+
 export default function HomeSlider() {
-  return <div>Home Slider</div>;
+  return (
+    <div className='absolute flex h-screen w-screen items-center justify-center'>
+      <Swiper
+        allowTouchMove={false}
+        spaceBetween={30}
+        effect={'fade'}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false
+        }}
+        speed={1000}
+        modules={[EffectFade, Autoplay]}
+        className='-z-10 size-full'
+      >
+        <SwiperSlide>
+          <Image src={camelsImage} alt='Camels' fill objectFit='cover' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={balconyImage} alt='Balcony' fill objectFit='cover' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={chaletImage} alt='Chalet' fill objectFit='cover' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={garlicImage} alt='Carlic' fill objectFit='cover' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={lampshadesImage} alt='Lampshades' fill objectFit='cover' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={skyImage} alt='Sky' fill objectFit='cover' />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
