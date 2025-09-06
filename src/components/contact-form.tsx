ef2c7d4157b9c41d3a311dbfc2ca5db0d514a57a
Dan Moffat <danmoffat@gmail.com>
2025-09-06 02:10:36 +0100

    A little form setup

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -2,7 +2,7 @@
 
 // TODO: Send the emeail
 // import sendMail from '@/actions/send-mail';
-import { useForm } from 'react-hook-form';
+import { type FieldValues, useForm } from 'react-hook-form';
 
 export default function ContactForm() {
   const {
@@ -11,29 +11,30 @@ export default function ContactForm() {
     formState: { errors, isSubmitting, isSubmitSuccessful }
   } = useForm();
 
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
   return (
-    <form
-      onSubmit={handleSubmit(data => console.log(data))}
-      className='flex flex-col items-center gap-4'
-    >
+    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col items-center gap-4'>
       <div className='flex items-center gap-2'>
         <label htmlFor='name'>Name</label>
         <input
-          {...register('name')}
+          {...register('name', { required: 'Please enter a name' })}
           type='text'
           name='name'
           id='name'
-          className='border border-zinc-50 p-2 outline-0 focus:border-2'
+          className='border border-zinc-300 p-2 outline-0 focus:border-2 dark:border-zinc-50'
         />
       </div>
       <div className='flex items-center gap-2'>
         <label htmlFor='email'>Email</label>
         <input
-          {...register('email')}
+          {...register('email', { required: 'Please enter a valid email address' })}
           type='email'
           name='email'
           id='email'
-          className='border border-zinc-50 p-2 outline-0 focus:border-2'
+          className='border border-zinc-300 p-2 outline-0 focus:border-2 dark:border-zinc-50'
         />
       </div>
       <div className='flex gap-2'>
@@ -41,10 +42,10 @@ export default function ContactForm() {
           Message
         </label>
         <textarea
-          {...register('message')}
+          {...register('message', { required: 'Please enter a message' })}
           name='message'
           id='message'
-          className='border border-zinc-50 p-2 outline-0 focus:border-2'
+          className='border border-zinc-300 p-2 outline-0 focus:border-2 dark:border-zinc-50'
         />
       </div>
       <button type='submit' className='width-full bg-zinc-200 p-2 text-zinc-900'>
