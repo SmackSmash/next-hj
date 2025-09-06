a97f8216dfe61d8ada5b68f871f13c57031bbd6e
Dan Moffat <danmoffat@gmail.com>
2025-09-06 02:42:10 +0100

    Some styling

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -11,44 +11,45 @@ export default function ContactForm() {
     formState: { errors, isSubmitting, isSubmitSuccessful }
   } = useForm();
 
-  const onSubmit = (data: FieldValues) => {
+  const onSubmit = async (data: FieldValues) => {
+    await new Promise(res => setTimeout(res, 1000));
     console.log(data);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col items-center gap-4'>
-      <div className='flex items-center gap-2'>
-        <label htmlFor='name'>Name</label>
-        <input
-          {...register('name', { required: 'Please enter a name' })}
-          type='text'
-          name='name'
-          id='name'
-          className='border border-zinc-300 p-2 outline-0 focus:border-2 dark:border-zinc-50'
-        />
-      </div>
-      <div className='flex items-center gap-2'>
-        <label htmlFor='email'>Email</label>
-        <input
-          {...register('email', { required: 'Please enter a valid email address' })}
-          type='email'
-          name='email'
-          id='email'
-          className='border border-zinc-300 p-2 outline-0 focus:border-2 dark:border-zinc-50'
-        />
-      </div>
-      <div className='flex gap-2'>
-        <label htmlFor='message' className='pt-2'>
-          Message
-        </label>
-        <textarea
-          {...register('message', { required: 'Please enter a message' })}
-          name='message'
-          id='message'
-          className='border border-zinc-300 p-2 outline-0 focus:border-2 dark:border-zinc-50'
-        />
-      </div>
-      <button type='submit' className='width-full bg-zinc-200 p-2 text-zinc-900'>
+    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-2'>
+      <label htmlFor='name'>Name</label>
+      <input
+        {...register('name', { required: 'Please enter a name' })}
+        type='text'
+        name='name'
+        id='name'
+        className='border border-zinc-300 p-2 outline-0 focus:border-zinc-600 dark:border-zinc-600 focus:dark:border-zinc-400'
+      />
+      <label htmlFor='email' className='mt-2'>
+        Email
+      </label>
+      <input
+        {...register('email', { required: 'Please enter a valid email address' })}
+        type='email'
+        name='email'
+        id='email'
+        className='border border-zinc-300 p-2 outline-0 focus:border-zinc-600 dark:border-zinc-600 focus:dark:border-zinc-400'
+      />
+      <label htmlFor='message' className='mt-2'>
+        Message
+      </label>
+      <textarea
+        {...register('message', { required: 'Please enter a message' })}
+        name='message'
+        id='message'
+        className='border border-zinc-300 p-2 outline-0 focus:border-zinc-600 dark:border-zinc-600 focus:dark:border-zinc-400'
+      />
+      <button
+        disabled={isSubmitting}
+        type='submit'
+        className='mt-2 w-full cursor-pointer bg-zinc-200 p-2 text-zinc-900 disabled:opacity-50'
+      >
         Send
       </button>
     </form>
