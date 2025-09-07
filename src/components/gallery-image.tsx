a47c9514f090c5e78da7962ccff34e58e280ba68
Dan Moffat <danmoffat@gmail.com>
2025-09-07 14:32:37 +0100

    Lazy load images

--- src/components/gallery-image.tsx
+++ src/components/gallery-image.tsx
@@ -6,6 +6,7 @@ export default function GalleryImage({ src, alt }: { src: StaticImageData; alt: 
       <Image
         src={src}
         alt={alt}
+        loading='lazy'
         className='max-h-screen w-fit'
         sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
       />
