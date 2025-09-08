f97c8418824a54a98045b35c592b7148956cd0f7
Dan Moffat <danmoffat@gmail.com>
2025-09-08 04:28:32 +0100

    Refinements

--- src/components/mode-switch.tsx
+++ src/components/mode-switch.tsx
@@ -20,25 +20,14 @@ export default function ThemeSwitch() {
     }
   };
 
-  if (!mounted)
+  if (mounted) {
     return (
-      <Image
-        src='data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=='
-        width={36}
-        height={36}
-        sizes='36x36'
-        alt='Loading Light/Dark Toggle'
-        priority={false}
-        title='Loading Light/Dark Toggle'
-      />
+      <div className='ml-auto flex items-center'>
+        <label htmlFor='dark-mode'>
+          {resolvedTheme === 'dark' ? <FiMoon className='size-5' /> : <FiSun className='size-5' />}
+        </label>
+        <Switch id='dark-mode' className='ml-4' onClick={handleClick} />
+      </div>
     );
-
-  return (
-    <div className='ml-auto flex items-center'>
-      <label htmlFor='dark-mode'>
-        {resolvedTheme === 'dark' ? <FiMoon className='size-5' /> : <FiSun className='size-5' />}
-      </label>
-      <Switch id='dark-mode' className='ml-4' onClick={handleClick} />
-    </div>
-  );
+  }
 }
