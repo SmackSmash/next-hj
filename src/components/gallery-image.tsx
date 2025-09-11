2d38581797b5439ddb6176df01ee6109709cfaea
Dan Moffat <danmoffat@gmail.com>
2025-09-11 10:40:10 +0100

    Improved styling

--- src/components/gallery-image.tsx
+++ src/components/gallery-image.tsx
@@ -25,9 +25,7 @@ export default function GalleryImage({ src, alt, images }: GalleryImageProps) {
           className={`group relative overflow-hidden rounded transition-all duration-1000 ${isLoaded ? 'translate-y-[0px] opacity-100' : 'translate-y-[15px] opacity-0'}`}
         >
           <div className='absolute top-0 left-0 z-10 flex size-full cursor-pointer items-center justify-center bg-zinc-950/75 opacity-0 backdrop-blur-xs backdrop-saturate-25 transition-opacity duration-300 ease-out group-hover:opacity-100'>
-            <p className='border-b-2 border-zinc-50 py-4 font-light tracking-wide text-zinc-50'>
-              View fullscreen
-            </p>
+            <p className='font-light tracking-wide text-zinc-50'>View fullscreen</p>
           </div>
           <Image
             src={src}
