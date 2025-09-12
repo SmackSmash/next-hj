c09666036fff4ba773a439a2903d195b05b5d5ed
Dan Moffat <danmoffat@gmail.com>
2025-09-12 17:21:40 +0100

    Reorder links

--- src/app/(site)/folios/page.tsx
+++ src/app/(site)/folios/page.tsx
@@ -9,14 +9,14 @@ export default function FoliosPage() {
   return (
     <div className='container mx-auto flex max-w-2xl flex-grow flex-col items-center justify-center gap-5 md:flex-row'>
       <div className='flex flex-col items-center justify-center gap-5 md:flex-grow'>
-        <Link href='/folios/travel' className='text-2xl font-light'>
-          Travel
-        </Link>
         <Link href='/folios/interiors' className='text-2xl font-light'>
           Interiors
         </Link>
         <Link href='/folios/food' className='text-2xl font-light'>
           Food
+        </Link>
+        <Link href='/folios/travel' className='text-2xl font-light'>
+          Travel
         </Link>
       </div>
       <div className='flex flex-col items-center justify-center gap-5 md:flex-grow'>
