2ffab067178058ae76c0efce299ad6b11b1ba43a
Dan Moffat <danmoffat@gmail.com>
2025-09-12 16:30:03 +0100

    Added folios menu

--- src/app/(site)/folios/page.tsx
+++ src/app/(site)/folios/page.tsx
@@ -7,13 +7,29 @@ export const metadata: Metadata = {
 
 export default function FoliosPage() {
   return (
-    <div>
-      <Link href='/folios/travel'>Travel</Link>
-      <Link href='/folios/interiors'>Interiors</Link>
-      <Link href='/folios/food'>Food</Link>
-      <Link href='/folios/lifestyle'>Lifestyle</Link>
-      <Link href='/folios/advertising'>Advertising</Link>
-      <Link href='/folios/social'>Social</Link>
+    <div className='container mx-auto flex max-w-2xl flex-grow flex-col items-center justify-center gap-5 md:flex-row'>
+      <div className='flex flex-col items-center justify-center gap-5 md:flex-grow'>
+        <Link href='/folios/travel' className='text-2xl font-light'>
+          Travel
+        </Link>
+        <Link href='/folios/interiors' className='text-2xl font-light'>
+          Interiors
+        </Link>
+        <Link href='/folios/food' className='text-2xl font-light'>
+          Food
+        </Link>
+      </div>
+      <div className='flex flex-col items-center justify-center gap-5 md:flex-grow'>
+        <Link href='/folios/lifestyle' className='text-2xl font-light'>
+          Lifestyle
+        </Link>
+        <Link href='/folios/advertising' className='text-2xl font-light'>
+          Advertising
+        </Link>
+        <Link href='/folios/social' className='text-2xl font-light'>
+          Social
+        </Link>
+      </div>
     </div>
   );
 }
